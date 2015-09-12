1a216d31a49c47050137ddf43cd109b552e1a0c8
swiffo <svegstrup@gmail.com>
2015-09-13 00:40:37 +0100

    God only knows

--- taxloss2.R
+++ taxloss2.R
@@ -1,16 +1,16 @@
 source('taxlosslib.R')
 
-do_harvest <- F
+do_harvest <- T
 
 capital_gains_tax <- 0.25
 
-tickers <- c('SPY', 'GDX')
+tickers <- c('SPY', 'GDX', 'EEM')
 intended_weights <- list()
 for(t in tickers)
   intended_weights[t] <- 1/length(tickers)
 start_value <- 1e6
 
-df <- multiple_ticker_close_values(tickers, 2005)
+df <- multiple_ticker_close_values(tickers, 2014)
 
 first_row <- df[1,]
 
@@ -81,24 +81,27 @@ calculate_total_value <- function(quantities, prices) {
 
 transact_in_ticker <- function(ticker, quantity, price) {
   history <- buy_history[[ticker]]
-  
+  print(sprintf('Transacting %f in %s (gl=%f)', quantity, ticker, -quantity*(price-history$price[idx])))
   if(quantity>0) {
     history$quantity <- c(history$quantity, quantity)
-    history$price <- c(history$price, price)
-  } else if(quantity<0) {
+    history$price    <- c(history$price, price)
+   } else if(quantity<0) {
     indices_to_remove <- NULL
     for(idx in length(history$quantity):1) {
-      q <- history$quantity[idx]
-      if(q > -quantity) {
-        history$quantity[idx] <- history$quantity[idx] + quantity
-        capital_gain_loss <<- capital_gain_loss - quantity*(price-history$price[idx]) # Global assignment
-        quantity <- 0
+      transaction_q <- history$quantity[idx]
+      transaction_p <- history$price[idx]
+      
+      if(transaction_q > -quantity) {
+        print(sprintf('partial sell in %s: %f of %f', ticker, quantity, q))
+        history$quantity[idx] <- transaction_q + quantity
+        capital_gain_loss <<- capital_gain_loss + (-quantity)*(price-transaction_p) # Global assignment
         break
       } else {
+        print(sprintf('full sell in %s: %f', ticker, quantity) )
         indices_to_remove <- c(indices_to_remove, idx)
-        capital_gain_loss <<- capital_gain_loss - quantity*(price-history$price[idx]) # Global assignment
-        quantity <- quantity + q
-      }
+        quantity <- quantity + transaction_q #remember quantity < 0; bring closer to 0
+        capital_gain_loss <<- capital_gain_loss + transaction_q*(price-transaction_p) # Global assignment      
+      }     
     }
     
     if(!is.null(indices_to_remove)) {
@@ -118,40 +121,43 @@ for(index in 1:dim(df)[1]) {
   tax_time <- do_taxes(current_date) #Remembers year so can only be called once per date
   
   if(do_reweighting(current_date, current_quantities) || tax_time) {
+    print(sprintf('Reweighing on %s', current_date))
+    
+    current_quantities <- calculate_quantities(buy_history)
+    current_weights    <- calculate_weights(current_quantities, current_date)
+    total_value        <- calculate_total_value(current_quantities, row)
+    
     if(do_harvest) {
-      current_quantities <- calculate_quantities(buy_history)
-      current_weights <- calculate_weights(current_quantities, current_date)
-      total_value <- calculate_total_value(current_quantities, row)
-      
       for(t in tickers) {
         # Take losses that can be taken
         current_price <- row[[t]]
         history <- buy_history[[t]]
         
         indices_to_remove <- NULL
+        to_rebuy_q <- 0
         for(idx in length(history$quantity):1) {
           buy_price <- history$price[idx]
-          to_rebuy <- 0
           if(buy_price >= current_price) {
             q <- history$quantity[idx]
-            to_rebuy <- to_rebuy + q*current_price
+            to_rebuy_q <- to_rebuy_q + q
             indices_to_remove <- c(indices_to_remove, idx)
             capital_gain_loss <- capital_gain_loss + q*(current_price-buy_price)
+            print(sprintf('rebalancing %s: gl=%f', t, q*(current_price-buy_price)))
           } else break
         }
         
-        if(to_rebuy > 0) {
-          history$quantity <- c(history$quantity[-indices_to_remove], to_rebuy/current_price)
+        if(to_rebuy_q > 0) {
+          history$quantity <- c(history$quantity[-indices_to_remove], to_rebuy_q)
           history$price <- c(history$price[-indices_to_remove], current_price)
         }
       }
-      
-      # Now we reweight
-      intended_quantity <- total_value * intended_weights[[t]] / row[[t]]
-      to_buy_quantity <- intended_quantity - current_quantities[[t]]
-      
-      transact_in_ticker(t, to_buy_quantity, current_price)
-    }
+    }  
+    # Now we reweight
+    intended_quantity <- total_value * intended_weights[[t]] / row[[t]]
+    to_buy_quantity <- intended_quantity - current_quantities[[t]]
+    
+    print(sprintf('to buy quantity: %f', to_buy_quantity))
+    transact_in_ticker(t, to_buy_quantity, current_price) 
   }
   
   if(tax_time){
@@ -172,10 +178,10 @@ for(index in 1:dim(df)[1]) {
    } 
   }
   
+  print(sprintf('gl on %s: %f', current_date, capital_gain_loss))
   # Calculate investment value
   quantities <- calculate_quantities(buy_history)
   tax_to_pay <- max(capital_gain_loss,0)*capital_gains_tax
-  print(sprintf('Accrued tax of %f on %s', tax_to_pay, current_date))
   total_value <- calculate_total_value(quantities, row) - tax_to_pay
   values <- c(values, total_value)
 }
